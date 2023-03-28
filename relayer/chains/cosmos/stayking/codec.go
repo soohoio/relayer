0c2ec9545b68a37cd0a06218c79b0083791a72c8
gloryan0829 <gloryan0829@icloud.com>
2023-03-28 13:50:57 +0900

    codec updated

--- relayer/chains/cosmos/stayking/codec.go
+++ relayer/chains/cosmos/stayking/codec.go
@@ -1,10 +1,12 @@
 package stayking
 
 import (
+	"github.com/cometbft/cometbft/proto/tendermint/crypto"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	proto "github.com/cosmos/gogoproto/proto"
 )
 
 // Needed for cosmos sdk Msg implementation in messages.go.
@@ -24,7 +26,16 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 	)
 }
 
+func RegisterCometTypes() {
+	proto.RegisterType((*crypto.Proof)(nil), "cometbft.crypto.Proof")
+	proto.RegisterType((*crypto.ValueOp)(nil), "cometbft.crypto.ValueOp")
+	proto.RegisterType((*crypto.DominoOp)(nil), "cometbft.crypto.DominoOp")
+	proto.RegisterType((*crypto.ProofOp)(nil), "cometbft.crypto.ProofOp")
+	proto.RegisterType((*crypto.ProofOps)(nil), "cometbft.crypto.ProofOps")
+}
+
 func init() {
+	RegisterCometTypes()
 	RegisterLegacyAminoCodec(amino)
 	cryptocodec.RegisterCrypto(amino)
 	amino.Seal()
